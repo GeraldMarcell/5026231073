f5a181f5da195b0cc85c4f8fea9a0f5cba544b92
Gerald Marcell <geraldmarcell65@gmail.com>
2025-05-22 15:48:36 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DosenController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
+
 // konsepnya kayak import java.io;
 
 // System.out.println("Hello World)";
@@ -67,3 +71,14 @@ Route::get('pertemuan7/validasi', function () {
 Route::get('pertemuan8/ets', function () {
     return view('index');
 });
+
+Route::get('dosen', [DosenController::class, 'index']);
+Route::get('welcome', [DosenController::class, 'welcome']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
